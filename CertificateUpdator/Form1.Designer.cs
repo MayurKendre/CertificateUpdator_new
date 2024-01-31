4743b3b025b1b64a055ad66130f46457992e2916
DELL <DELL@DESKTOP-B9K1KTI>
2024-01-31 17:15:18 +0530

    Add project files.

---
+++ CertificateUpdator/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace CertificateUpdator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlBackground = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblBackground = new System.Windows.Forms.Label();
+            this.btnBackground = new System.Windows.Forms.Button();
+            this.ctrlContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.excelContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.pnlBackground, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pnlBackground
+            // 
+            this.pnlBackground.ContextMenuStrip = this.ctrlContextMenu;
+            this.pnlBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlBackground.Location = new System.Drawing.Point(23, 23);
+            this.pnlBackground.Name = "pnlBackground";
+            this.pnlBackground.Size = new System.Drawing.Size(754, 338);
+            this.pnlBackground.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.lblBackground, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnBackground, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(23, 367);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(754, 80);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // lblBackground
+            // 
+            this.lblBackground.AutoSize = true;
+            this.lblBackground.Location = new System.Drawing.Point(10, 10);
+            this.lblBackground.Margin = new System.Windows.Forms.Padding(10);
+            this.lblBackground.Name = "lblBackground";
+            this.lblBackground.Size = new System.Drawing.Size(65, 13);
+            this.lblBackground.TabIndex = 0;
+            this.lblBackground.Text = "Background";
+            // 
+            // btnBackground
+            // 
+            this.btnBackground.Location = new System.Drawing.Point(90, 5);
+            this.btnBackground.Margin = new System.Windows.Forms.Padding(5);
+            this.btnBackground.Name = "btnBackground";
+            this.btnBackground.Size = new System.Drawing.Size(75, 23);
+            this.btnBackground.TabIndex = 1;
+            this.btnBackground.Text = "...";
+            this.btnBackground.UseVisualStyleBackColor = true;
+            this.btnBackground.Click += new System.EventHandler(this.btnBackground_Click);
+            // 
+            // ctrlContextMenu
+            // 
+            this.ctrlContextMenu.Name = "ctrlContextMenu";
+            this.ctrlContextMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // excelContextMenu
+            // 
+            this.excelContextMenu.Name = "excelContextMenu";
+            this.excelContextMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel pnlBackground;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label lblBackground;
+        private System.Windows.Forms.Button btnBackground;
+        private System.Windows.Forms.ContextMenuStrip ctrlContextMenu;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.ContextMenuStrip excelContextMenu;
+    }
+}
+
---
+++ CertificateUpdator/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CertificateUpdator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AddContextMenu();
+            AddExcelHeadersToContextMenu();
+        }
+
+        /// <summary>
+        /// Read headers from excel and add to context menu
+        /// </summary>
+        private void AddExcelHeadersToContextMenu()
+        {
+            excelContextMenu.Items.Add("Name");
+            excelContextMenu.Items.Add("Phone Number");
+            excelContextMenu.Items.Add("Image");
+
+        }
+
+        /// <summary>
+        /// Add controls to context menu
+        /// </summary>
+        private void AddContextMenu()
+        {
+            ToolStripMenuItem Item = new ToolStripMenuItem("Text Box", Image.FromFile(@"..\..\images\text-box.png"));
+            Item.Click += TextBoxClicked;
+            ctrlContextMenu.Items.Add(Item);
+
+            Item = new ToolStripMenuItem("Image", Image.FromFile(@"..\..\images\text-box.png"));
+            Item.Click += ImageClicked;
+            ctrlContextMenu.Items.Add(Item);
+        }
+
+
+        /// <summary>
+        /// Context menu image is clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ImageClicked(object sender, EventArgs e)
+        {
+            PictureBox picBx = new PictureBox();
+            picBx.BackColor = System.Drawing.SystemColors.ControlDark;
+            ControlExtension.Draggable(picBx,true);
+            pnlBackground.Controls.Add(picBx);
+            AddExcelContextMenu(picBx);
+        }
+
+        /// <summary>
+        /// Context menu Text box is clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBoxClicked(object sender, EventArgs e)
+        {
+            TextBox txtBx = new TextBox();
+            ControlExtension.Draggable(txtBx, true);
+            pnlBackground.Controls.Add(txtBx);
+            AddExcelContextMenu(txtBx);
+
+        }
+
+        /// <summary>
+        /// Add background Image to panel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBackground_Click(object sender, EventArgs e)
+        {
+            DialogResult result =  openFile.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                pnlBackground.BackgroundImageLayout = ImageLayout.Zoom;
+                pnlBackground.BackgroundImage = Image.FromFile(openFile.FileName);
+            }
+        }
+
+        /// <summary>
+        /// Add context menu to controls
+        /// </summary>
+        /// <param name="ctrl"></param>
+        private void AddExcelContextMenu(Control ctrl)
+        {
+            ctrl.ContextMenuStrip = excelContextMenu;
+        }
+    }
+}
